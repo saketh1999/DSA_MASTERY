e0b3dd3605e1e6187f4e98bac0cd17776709b0f8
saketh1999 <48793201+saketh1999@users.noreply.github.com>
2024-01-02 08:46:33 -0800

    Time: 27 ms (28.19%) | Memory: 17.7 MB (98.97%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int cookiesNums = s.size();
-        if(cookiesNums == 0)  return 0;
-        sort(g.begin(), g.end());
-        sort(s.begin(), s.end());
+        if(s.size()==0)
+        return 0;
+        int greedIdx = g.size()-1;
+        int cookiesIdx = s.size()-1;
 
-        int maxNum = 0;
-        int cookieIndex = cookiesNums - 1;
-        int childIndex = g.size() - 1;
-        while(cookieIndex >= 0 && childIndex >=0){
-            if(s[cookieIndex] >= g[childIndex]){
-                maxNum++;
-                cookieIndex--;
-                childIndex--;
+        //Sort the cookies and greed factors
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+
+        int children=0;
+        while(greedIdx>=0 && cookiesIdx>=0)
+        {
+            if(s[cookiesIdx]>=g[greedIdx])
+            {
+                children++;
+                cookiesIdx--;
+                greedIdx--;
             }
-            else{
-                childIndex--;
+            else
+            {
+                //Since the cookies at other index can be used to satisfy the 
+                //greed of other children
+                greedIdx--;
             }
         }
-        return maxNum;
+        return children;
+
+        
     }
 };
