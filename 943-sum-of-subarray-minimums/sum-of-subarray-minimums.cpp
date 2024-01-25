3446086285ee756ab063b0f92a09c0ca0980c36e
saketh1999 <48793201+saketh1999@users.noreply.github.com>
2024-01-25 01:35:28 -0800

    Time: 69 ms (34.89%) | Memory: 49.3 MB (5.06%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,36 +1,45 @@
 class Solution {
 public:
-int sumSubarrayMins(vector<int>& arr) {
-    stack<pair<int,int>> s1; //for left fill
-    stack<pair<int,int>> s2; // for right fill
-    vector<long> left(arr.size());
-    vector<long> right(arr.size());
-    for(int i=0;i<arr.size();++i){
-                int c=1;
-                while(!s1.empty() && s1.top().first >= arr[i]){
-                 c+=s1.top().second;
-                s1.pop();
-                }
-                s1.push({arr[i],c});
-                left[i] = c;
-        }
-    
-     for(int i=arr.size()-1;i>=0;i--){
-         int c=1;
-                while(!s2.empty() && s2.top().first > arr[i]){
-                 c+=s2.top().second;
-                s2.pop();
-                }
-                s2.push({arr[i],c});
-                right[i] = c;
-    }
-    long int sum=0;
-    for(int i=0;i<arr.size();++i){
-        long int c = arr[i];
-        sum+=(c*left[i]*right[i]);
-             sum=sum % (1000000000 + 7);
+    int sumSubarrayMins(vector<int>& arr) {
+        long long  mod = 1e9+7;
+        int n  = arr.size();
+        
+        vector<long long> left(n,0),right(n,0);
 
+        stack<pair<int,int>> sRight,sLeft;
+
+        for(int i =0;i<arr.size();i++)
+        {
+            int count = 1;
+            while(!sLeft.empty() && sLeft.top().first>=arr[i])
+            {
+                count+=sLeft.top().second ; //Adding the count of the first element also
+                sLeft.pop();
+            }
+            sLeft.push({arr[i],count});
+            left[i] = count;
+        }
+        
+        for(int i =n-1;i>=0;i--)
+        {
+            int count = 1;
+            while(!sRight.empty() && sRight.top().first>arr[i])
+            {
+                count+=sRight.top().second ; //Adding the count of the first element also
+                sRight.pop();
+            }
+            sRight.push({arr[i],count});
+            right[i] = count;
+        }
+        
+        long long int res = 0 ;
+        for(int i=0;i<arr.size();i++)
+        {
+            
+            res += (arr[i]*left[i]*right[i]);
+            res = res%mod;
+            
+        }
+        return res;
     }
-    return sum;
-}
 };
