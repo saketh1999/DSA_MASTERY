ee05c37a3316df1234b98a4eec7713b2b58c64ec
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2024-09-15 16:57:07 -0700

    Time: 161 ms (64.17%) | Memory: 18.6 MB (33.85%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.py
@@ -0,0 +1,33 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        
+        if len(hand)%groupSize != 0 :
+            return False
+
+        ##Creating a Dictionary
+        myDict= {}
+        for x in hand:
+            if x in myDict:
+                myDict[x] +=1
+            else:
+                myDict[x] = 1
+
+        ##Creating a min heap for the keys
+        myheap = list(myDict.keys())
+        heapq.heapify(myheap) #min heap
+
+        ##Traversing the hand
+        while myheap:
+            first = myheap[0]
+
+            for i in range(first,first + groupSize):
+                if i not in myDict:
+                    return False
+
+                myDict[i] -= 1
+                if myDict[i] == 0:
+                    if i != myheap[0]:
+                        return False
+                    heapq.heappop(myheap)
+
+        return True
