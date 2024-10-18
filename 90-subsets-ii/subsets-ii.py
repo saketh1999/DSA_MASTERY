e822cfc0bc12ed1359a97da3ab2d15d981a39daa
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2024-10-18 14:27:27 -0700

    Time: 0 ms (100.00%) | Memory: 16.9 MB (5.95%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,9 +1,7 @@
 class Solution:
     def rec_sub(self,nums,index,res,curr_res):
         if index >= len(nums):
-            # curr_res.sort()
-            res.append(curr_res.copy())
-
+            res.add(tuple(curr_res.copy()))
             return 
         
         
@@ -17,18 +15,9 @@ class Solution:
         return
         
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        res =[]
-        final_res = []
-        myTempSet = set()
+        res =set()
         nums.sort()
 
         self.rec_sub(nums,0,res,[])
 
-        for r in res:
-            myTempSet.add(tuple(r))
-        for x in myTempSet:
-            final_res.append(x)
-
-
-
-        return final_res
+        return list(res)
