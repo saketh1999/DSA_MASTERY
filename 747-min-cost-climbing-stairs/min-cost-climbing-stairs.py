21f7c6aafcd6269fbad87979fabed3d027a4a5c3
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2024-11-20 23:59:47 -0800

    Time: 6 ms (22.90%) | Memory: 18.2 MB (16.00%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -2,6 +2,7 @@ class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         n = len(cost)
         stair_cost = [-1]*(n+1)
+        #Bottom up
         def climb(n):
             if n == 0:
                 return cost[0]
@@ -15,4 +16,15 @@ class Solution:
             stair_cost[n] = cost[n] + min(climb(n-1),climb(n-2))
             return stair_cost[n]
         
-        return min(climb(n-1),climb(n-2))
+        #Top Down
+        def climb_TD(n):
+            stair_cost[0] = cost[0]
+            stair_cost[1] = cost[1]
+            
+            print(stair_cost)
+            for i in range(2,n):
+
+                stair_cost[i] = min(stair_cost[i-1],stair_cost[i-2]) + cost[i]
+
+            return min(stair_cost[n-1],stair_cost[n-2])
+        return climb_TD(n)
