5de2e1cf847a4e232b9cc85d7a5a0e01c937b8b0
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2024-11-25 12:25:06 -0800

    Time: 256 ms (38.63%) | Memory: 19.2 MB (46.69%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,26 +1,36 @@
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        row= len(grid)
-        col = len(grid[0])
-        visited = [[0 for i in range(col)] for j in range(row)]
-        count = 0
+    def numIslands(self, matrix: List[List[str]]) -> int:
+        m=len(matrix)
+        n=len(matrix[0])
 
-        def dfs(i,j):
-            if i<0 or j<0 or i>=row or j>= col or grid[i][j]=="0" or visited[i][j]==1:
-                return 
-
-            visited[i][j] = 1
-
-            dfs(i+1,j)
-            dfs(i-1,j)
-            dfs(i,j+1)
-            dfs(i,j-1)
         
-        for i in range(row):
-            for j in range(col):
-                if grid[i][j] == "1" and visited[i][j]==0:
-                    count+=1
-                    dfs(i,j)
+        def dfs(matrix,visited,row,col):
+            matrix_row = len(matrix)
+            matrix_col = len(matrix[0])
 
+        
+
+            visited[row][col] = 1 #marking visited
+
+            direction = [[0,1],[-1,0],[1,0],[0,-1]] #this indicated all the four directions we want to find the islands
+
+            for dr,dc in direction:
+                nr,nc = row+dr,col+dc
+                #validity check of the row and col
+                if nr < matrix_row and nr>=0 and nc>=0 and nc<matrix_col :
+                    if visited[nr][nc]==0 and matrix[nr][nc]=="1":
+                        #check for neigh
+                        dfs(matrix,visited,nr,nc)
+        
+        visited = [[0 for i in range(n)] for j in range(m)]
+        # print(visited)
+        count=0
+        for i in range(m):
+            for j in range(n):
+                if visited[i][j]==0 and matrix[i][j]=="1": #unvisited island
+                    count+=1
+                    dfs(matrix,visited,i,j)
+        
         return count
-                
+
+        
