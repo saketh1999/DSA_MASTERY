98dac7b8eff742c265dee50979c15d2d6bfd66c6
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2024-11-27 11:45:39 -0800

    Time: 0 ms (100.00%) | Memory: 17 MB (7.52%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -2,9 +2,10 @@ class Solution:
     def hammingWeight(self, n: int) -> int:
         cnt=0
         while n:
-            print(fmod(n,2))
-            if fmod(n,2)==1:
+
+            if n&1:
                 cnt+=1
-            n = n//2
-        # if n==1: cnt+=1
+            n = n>>1
+
+
         return cnt
