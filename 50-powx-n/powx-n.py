b53221d5c001783f0f58c40e7a68b419be78b58c
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2024-11-28 10:40:35 -0800

    Time: 0 ms (100.00%) | Memory: 17.2 MB (5.40%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -1,3 +1,19 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return math.pow(x,n)
+        
+        def helper(x,n):
+            if x==0:
+                return 0
+            if n==0:
+                return 1
+            
+            res = helper(x,n//2)
+            res*=res
+            if n&1 == True:
+                res*=x
+            return res
+
+        res = helper(x,abs(n))#just finding the normal x^n    
+        if n<0:
+            return 1/res
+        return res
