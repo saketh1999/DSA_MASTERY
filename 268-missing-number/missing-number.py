f310f59672a2cdf300709c3489e5c99e68adba3a
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2024-11-27 18:23:35 -0800

    Time: 7 ms (39.47%) | Memory: 18.3 MB (6.48%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -2,11 +2,16 @@ class Solution:
     def missingNumber(self, nums: List[int]) -> int:
 
         n = len(nums)
+
+        #xor the first n elements
         check1 = 0
         for i in range(1,n+1):
             check1^=i
+        
+        #xor the elements in the array
         check2 = nums[0]
         for i in range(1,n):
             check2^=nums[i]
-        print
+
+        #xor the result
         return check1^check2
