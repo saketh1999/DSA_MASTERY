4cbe48b1bc65314de09cdd2056e54d17f9873135
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2024-11-29 22:51:35 -0800

    Time: 56 ms (77.22%) | Memory: 20.4 MB (25.49%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,28 +1,41 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        #checking if the endWord is in the Wordlist
         if endWord not in wordList:
             return 0
 
-        nei = collections.defaultdict(list)
+        #create an Adj list for the words
+
+        #adding begin word to the wordList
         wordList.append(beginWord)
+
+        adjList = defaultdict(list)
         for word in wordList:
             for j in range(len(word)):
-                pattern = word[:j] + "*" + word[j + 1 :]
-                nei[pattern].append(word)
-
-        visit = set([beginWord])
-        q = deque([beginWord])
-        res = 1
+                pattern = word[:j]+'*'+word[j+1:]
+                adjList[pattern].append(word)
+        
+        #Now do a simple bfs
+        visited = set()
+        q=deque()
+        visited.add(beginWord)
+        q.append(beginWord)
+        res=1
         while q:
             for i in range(len(q)):
-                word = q.popleft()
+                word = q[0]
+                q.popleft()
+
                 if word == endWord:
                     return res
+                
                 for j in range(len(word)):
-                    pattern = word[:j] + "*" + word[j + 1 :]
-                    for neiWord in nei[pattern]:
-                        if neiWord not in visit:
-                            visit.add(neiWord)
-                            q.append(neiWord)
-            res += 1
+                    pattern = word[:j]+"*"+word[j+1:]
+                    for nei in adjList[pattern]:
+                        if nei not in visited:
+                            q.append(nei)
+                            visited.add(nei)
+            res+=1
+
         return 0
+
