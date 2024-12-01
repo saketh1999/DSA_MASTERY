1cbcdd9a4954a25fbf8eb6a43846c3ea954946e9
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2024-12-01 15:15:07 -0800

    Time: 3 ms (57.78%) | Memory: 17.3 MB (6.92%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.py
@@ -4,26 +4,25 @@ class Solution:
 
         #first sort the array
         arr.sort()
-        elements_so_far =[]
+
         curr=0
         
         while curr<len(arr):
-            left,right=curr+1,len(arr)-1
+            left,right=0,len(arr)-1
             
             target = 2*arr[curr]
-            if target in elements_so_far:
-                return True
+        
             while left<=right:
                 mid = (left+right)//2
 
-                if arr[mid]==target:
+                if arr[mid]==target and curr!=mid:
                     return True
                 
                 elif arr[mid]>target:
                     right=mid-1
                 else:
                     left=mid+1
-            elements_so_far.append(arr[curr])
+
             curr+=1
         return False
             
