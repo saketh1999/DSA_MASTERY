9b5317c9d2a8160d9080a935ba58be13d0d61ca4
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2024-12-01 11:48:14 -0800

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.py
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.py
@@ -1,52 +1,47 @@
 from typing import List
-from collections import deque
 class Solution:
     #Function to detect cycle in an undirected graph.
 	def isCycle(self, V: int, adj: List[List[int]]) -> bool:
 		#Code here
-		visited = [0 for _ in range(V)]
-		                
-		def dfs_cycle_detection(node,parent):
+		visited = [0]*V
+		def dfs(node,parent):
 		    
-		    visited[node] = 1
+		    visited[node]=1
 		    
-		    for n in adj[node]:
-		        if visited[n] == 0:
-		            if dfs_cycle_detection(n,node):
+		    for nei in adj[node]:
+		        if visited[nei]==0:
+		            if dfs(nei,node) == True:
 		                return True
-		                
-		        elif parent!=n:
-		            
+		        elif nei != parent:
 		            return True
-		            
 		    return False
-		                
+		
 		for i in range(V):
-		     if visited[i]==0:
-		         if dfs_cycle_detection(i,-1):
-		             return True
-		        
+		    if visited[i]==0:
+		        if dfs(i,-1) == True:
+		            return True
+		return False
 		    
-        return False
-    
+
+
 #{ 
  # Driver Code Starts
-
 if __name__ == '__main__':
 
-	T=int(input())
-	for i in range(T):
-		V, E = map(int, input().split())
-		adj = [[] for i in range(V)]
-		for _ in range(E):
-			u, v = map(int, input().split())
-			adj[u].append(v)
-			adj[v].append(u)
-		obj = Solution()
-		ans = obj.isCycle(V, adj)
-		if(ans):
-			print("1")
-		else:
-			print("0")
+    T = int(input())
+    for i in range(T):
+        V, E = map(int, input().split())
+        adj = [[] for i in range(V)]
+        for _ in range(E):
+            u, v = map(int, input().split())
+            adj[u].append(v)
+            adj[v].append(u)
+        obj = Solution()
+        ans = obj.isCycle(V, adj)
+        if (ans):
+            print("1")
+        else:
+            print("0")
+        print("~")
 
 # } Driver Code Ends
