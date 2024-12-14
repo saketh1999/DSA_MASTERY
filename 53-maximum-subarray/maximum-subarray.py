115c92a099f026d3a4efcb9125a2b524bdb3f0d8
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2024-12-14 07:40:47 -0800

    Time: 58 ms (61.30%) | Memory: 31.7 MB (10.25%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -4,11 +4,10 @@ class Solution:
         sum_so_far = 0
         for i in range(len(nums)):
             sum_so_far += nums[i]
-
-
-
+            
             max_val = max(max_val,sum_so_far)
             if sum_so_far < 0:
                 sum_so_far = 0
+            
         
         return max_val
