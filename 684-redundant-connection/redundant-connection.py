32cfc35fc198a68325f44f5d5006146ef5ec5c97
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2024-12-18 17:16:38 -0800

    Time: 0 ms (100.00%) | Memory: 18.2 MB (5.55%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,31 +1,34 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        n = len(edges)
-        parent = [i for i in range(n+1)]
-        rank = [0 for i in range(n+1)]
+        parent = [i for i in range(len(edges)+1)]
+        rank = [0 for i in range(len(edges)+1)]
 
         def find(node):
             p = parent[node]
-            while p!= parent[p]:
-                parent[p] = parent[parent[p]]
-                p = parent[p]
-            return p
-        
-        def union(node1,node2):
-            p1,p2 = find(node1),find(node2)
 
-            if p1 == p2:
+            while p!=parent[p]:
+                parent[p] = parent[parent[p]]
+                p =parent[p]
+            
+            return p
+        def union(node1,node2):
+            p1,p2=find(node1),find(node2)
+
+            if p1==p2:
                 return False
             
             if rank[p1] > rank[p2]:
                 parent[p2]=p1
-            elif rank[p1] < rank[p2]:
-                parent[p1]=p2
-            else:
-                parent[p1]=p2
-                rank[p2]+=1
+            elif rank[p2] > rank[p1]:
+                parent[p1] = p2
+            else :
+                parent[p1] = p2
+                rank[p2] +=1
+            
             return True
         
         for u,v in edges:
-            if  union(u,v) == False:
+            if union(u,v) == False:
                 return [u,v]
+
+            
