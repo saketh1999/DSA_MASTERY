04ec1da267ec03a56455be8896d04e3327d60175
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2025-01-09 16:19:17 -0800

    Time: 43 ms (5.73%) | Memory: 18.8 MB (9.50%) - LeetSync

--- 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.py
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.py
@@ -1,11 +1,36 @@
+class PrefixNode:
+    def __init__(self):
+        self.children = {}
+        self.count=0
+
+class PrefixTree:
+    def __init__(self):
+        self.root = PrefixNode()
+    def addWord(self,w):
+        curr = self.root
+        for c in w:
+            if c not in curr.children:
+                curr.children[c]=PrefixNode()
+            curr = curr.children[c]
+            curr.count+=1
+
+    def countWords(self,pref):
+        curr = self.root
+        for c in pref:
+            if c not in curr.children:
+                return 0
+            
+            curr=curr.children[c]
+        return curr.count
+
+            
+            
+            
 class Solution:
     def prefixCount(self, words: List[str], pref: str) -> int:
-        count = 0
-        N = len(pref)
+        prefixTree = PrefixTree()
         for w in words:
-            if N >len(w):
-                continue
-            if w[:N] == pref:
-                count+=1
-        
-        return count
+                if len(w)>=len(pref):
+                    prefixTree.addWord(w)
+            
+        return prefixTree.countWords(pref)
