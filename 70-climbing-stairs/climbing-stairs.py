f31f17ee269fcc436447958a019554fbf34e84aa
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2025-02-01 00:23:04 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (23.20%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,41 +1,13 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        #Top Down Approach
-        stairs = [-1]*(n+1)
-
-        def climb(n):
-            if n == 0:
+        dp = [0]*(n+1)
+        def helper(n):
+            if n==0 or n==1:
                 return 1
-            if n==1:
-                return 1
+            if dp[n]:
+                return dp[n]
 
-            if stairs[n]!=-1:
-                return stairs[n]
-            stairs[n] = climb(n-1)+climb(n-2)
-            return stairs[n]
-        
-        #Bottom Up Approach
-        def climbTD(n):
-            stairs = [-1]*(n+1)
-            stairs[0]=1
-            stairs[1]=1
-            for i in range(2,n+1):
-             stairs[i]=stairs[i-1]+stairs[i-2]
+            dp[n] = helper(n-1)+helper(n-2)
+            return dp[n]
 
-            
-            return stairs[n]
-
-        def climbTD_SpaceOptimized(n):
-
-            prev2Stair = 1
-            prevStair = 1
-            for i in range(2,n+1):
-             curr = prev2Stair+prevStair
-
-             prev2Stair=prevStair
-             prevStair=curr
-
-            
-            return prevStair
-        
-        return climbTD_SpaceOptimized(n)
+        return helper(n)
