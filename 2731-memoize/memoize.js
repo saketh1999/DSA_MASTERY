32818e781497affb208a52e843f07b0aac062488
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2025-02-20 22:04:58 -0800

    Time: 288 ms (63.38%) | Memory: 94.4 MB (5.12%) - LeetSync

---
+++ 2731-memoize/memoize.js
@@ -0,0 +1,29 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    const cache = {}
+    return function(...args) {
+        let key  = JSON.stringify(args)
+        
+        if (key in cache){
+            return cache[key]
+        }
+        let output = fn(...args)
+        cache[key]=output
+        return cache[key]
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
