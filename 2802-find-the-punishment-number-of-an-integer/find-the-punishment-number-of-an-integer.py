576fe0fb2458ca5adfce0259c92dded47a7caffa
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2025-02-23 18:06:17 -0800

    Time: 1351 ms (14.91%) | Memory: 18 MB (33.28%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.py
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.py
@@ -1,29 +1,19 @@
 class Solution:
-    def can_partition(self, num, target):
-        # Invalid partition found
-        if target < 0 or num < target:
-            return False
-
-        # Valid partition found
-        if num == target:
-            return True
-
-        # Recursively check all partitions for a valid partition
-        return (
-            self.can_partition(num // 10, target - num % 10)
-            or self.can_partition(num // 100, target - num % 100)
-            or self.can_partition(num // 1000, target - num % 1000)
-        )
-
     def punishmentNumber(self, n: int) -> int:
-        punishment_num = 0
+        res = 0
+        def partition(i:int,curr:int,target:int,string:str):
+            if i==len(string) and curr == target:
+                return True
+            
+            for j in range(i,len(string)):
+                string[i:j+1]
+                if partition(j+1,curr+int(string[i:j+1]),target,string):
+                    return True
+            return False
+        
+        for i in range(1,n+1):
+            if partition(0,0,i,str(i*i)):
+                res+=i*i
 
-        # Iterate through numbers in range [1, n]
-        for current_num in range(1, n + 1):
-            square_num = current_num * current_num
 
-            # Check if valid partition can be found and add squared number if so
-            if self.can_partition(square_num, current_num):
-                punishment_num += square_num
-
-        return punishment_num
+        return res
