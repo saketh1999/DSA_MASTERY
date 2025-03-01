f3fabfb8a49f10fdf413c8c7e65b2e77dee12f38
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2025-02-28 19:01:39 -0800

    Time: 0 ms (100.00%) | Memory: 18 MB (12.83%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -5,15 +5,31 @@ class Solution:
         def helper(i):
             if i<0:
                 return 0
-
-            # if i==0 or i==1:
-            #     return nums[i]
             
             if dp[i]!=-1:
                 return dp[i]
+
             take = nums[i] + helper(i-2)
             dont_take = helper(i-1)
             dp[i] = max(take,dont_take)
         
             return dp[i]
-        return max(helper(n-1) , helper(n-2))
+        # return max(helper(n-1) , helper(n-2))
+
+        def helper_bt_up():
+           
+            if n == 1:
+                return nums[0]
+            
+            dp[0] = nums[0]
+
+
+            for i in range(1,n):
+                take = nums[i] 
+                if i>1:
+                    take+=dp[i-2]
+                dont_take = dp[i-1]
+                dp[i] = max(take,dont_take)
+            
+            return dp[n-1]
+        return helper_bt_up()
