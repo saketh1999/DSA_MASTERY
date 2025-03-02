bfc06e247bf4da67c6d678ffcba3a0b03c702d47
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2025-03-02 11:34:35 -0800

    Time: 3 ms (73.55%) | Memory: 19.2 MB (17.02%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,30 +1,14 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n = len(cost)
-        stair_cost = [-1]*(n+1)
-        #Bottom up
-        def climb(n):
-            if n == 0:
-                return cost[0]
-
-            if n == 1:
-                return cost[1]
-
-            if stair_cost[n]!=-1:
-                return stair_cost[n]
-
-            stair_cost[n] = cost[n] + min(climb(n-1),climb(n-2))
-            return stair_cost[n]
-        
-        #Top Down
-        def climb_TD(n):
-            stair_cost[0] = cost[0]
-            stair_cost[1] = cost[1]
+        n=len(cost)
+        dp = [-1] * (n+1)
+        def helper(n):
+            if n<0:
+                return 0
+            if dp[n]!=-1:
+                return dp[n]
             
-            print(stair_cost)
-            for i in range(2,n):
-
-                stair_cost[i] = min(stair_cost[i-1],stair_cost[i-2]) + cost[i]
-
-            return min(stair_cost[n-1],stair_cost[n-2])
-        return climb_TD(n)
+            dp[n] = cost[n]+ min(helper(n-1),helper(n-2))
+            return dp[n]
+        
+        return min(helper(n-1),helper(n-2))
