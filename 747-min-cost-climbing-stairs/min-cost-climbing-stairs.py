9852d907ed0bc7137599559070634c22242c9106
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2025-03-02 11:45:59 -0800

    Time: 9 ms (8.35%) | Memory: 19.5 MB (15.69%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -11,4 +11,13 @@ class Solution:
             dp[n] = cost[n]+ min(helper(n-1),helper(n-2))
             return dp[n]
         
-        return min(helper(n-1),helper(n-2))
+        # return min(helper(n-1),helper(n-2))
+
+        def helper_BU():
+            dp[0]=cost[0]
+            dp[1] = cost[1]
+            for i in range(2,n):
+                dp[i] = cost[i] + min(dp[i-1],dp[i-2])
+            print(dp)
+            return min(dp[n-1],dp[n-2])
+        return helper_BU()
