4c5da18669eec8770b80693278d74c40af9de86c
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2025-03-08 08:29:39 -0800

    Time: 100 ms (5.04%) | Memory: 34.9 MB (31.80%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.py
@@ -1,18 +1,21 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        less = []
-        equal = []
-        greater = []
+        i,j=0,len(nums)-1 #traverse the arr
+        i2,j2=0,len(nums)-1 # pointer to add the numbers to the new arr
+        res = [0]*len(nums)
 
-        for num in nums:
-            if num < pivot:
-                less.append(num)
-            elif num > pivot:
-                greater.append(num)
-            else:
-                equal.append(num)
-
-        less.extend(equal)
-        less.extend(greater)
-
-        return less
+        while i <= len(nums)-1:
+            if nums[i]<pivot:
+                res[i2] = nums[i]
+                i2+=1
+            if nums[j]>pivot:
+                res[j2] = nums[j]
+                j2-=1
+                
+            i,j=i+1,j-1
+        
+        while i2<=j2:
+            res[i2]=pivot
+            i2+=1
+        
+        return res
