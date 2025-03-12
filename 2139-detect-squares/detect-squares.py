494fdb4168a3b2b976b5ee30574ac87230f5575f
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2025-03-11 18:34:10 -0700

    Time: 293 ms (7.23%) | Memory: 19.6 MB (23.41%) - LeetSync

--- 2139-detect-squares/detect-squares.py
+++ 2139-detect-squares/detect-squares.py
@@ -1,20 +1,25 @@
 class DetectSquares:
 
     def __init__(self):
-        self.point_dic = defaultdict(int)
         self.points = []
+        self.dict = defaultdict(int)
 
     def add(self, point: List[int]) -> None:
         self.points.append(point)
-        self.point_dic[tuple(point)]+=1
+        if tuple(point) not in self.dict:
+            self.dict[tuple(point)]=0
+        self.dict[tuple(point)]+=1
+
 
     def count(self, point: List[int]) -> int:
         res = 0
         px,py=point
         for x,y in self.points:
-            if (abs(px-x) != abs(py-y) )or px==x or py==y:
+            if abs(px-x)!=abs(py-y) or x==px or y==py:
                 continue
-            res +=  self.point_dic[(x,py)] * self.point_dic[(px,y)]
+            res+= self.dict[tuple([x,py])] * self.dict[tuple([px,y])]
+        
+
         return res
 
 
