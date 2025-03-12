98ba829884192ef769183136f4f66ebb6c1fa1c9
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2025-03-11 19:55:54 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (17.35%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,8 +1,7 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        dp1 = [-1]*(n+1)
-        dp2 = [-1]*(n+1)
+        dp = [-1]*(n+1)
 
         if n == 1:
             return nums[0]
@@ -22,7 +21,11 @@ class Solution:
             dp[h] = max(pick,d_pick)
             return dp[h]
 
-        arr1 = nums[1:n]
-        arr2 = nums[0:n-1]
+        # arr1 = nums[1:n]
+        # arr2 = nums[0:n-1]
 
-        return max(helper(n-2,arr1,dp1),helper(n-2,arr2,dp2))
+        max1 = helper(n-2,nums[1:n],dp)
+        dp = [-1]*(n+1)
+        max2 = helper(n-2,nums[0:n-1],dp)
+
+        return max(max1,max2)
