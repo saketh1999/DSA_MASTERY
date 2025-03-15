274d9e36cc007bb959afdd43a35790c5625c7d6e
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2025-03-14 20:52:30 -0700

    Time: 2801 ms (5.01%) | Memory: 20.6 MB (84.59%) - LeetSync

---
+++ 15-3sum/3sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = set([])
+        target=0
+
+        for i in range(len(nums)):
+            my_set = set([])
+            my_tar = target - nums[i]
+            for j in range(i+1,len(nums)):
+                if nums[j] in my_set :
+                    ans = [nums[i],nums[j],(my_tar-nums[j])]
+                    ans.sort()
+                    res.add(tuple(ans))
+                
+                else:
+                    
+                    my_set.add(my_tar - nums[j])
+        
+        return list(res)
