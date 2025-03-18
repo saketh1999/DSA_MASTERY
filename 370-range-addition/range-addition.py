4540c3656411e2f73a8310d47fff73a7bf3d8cb2
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2025-03-17 20:03:25 -0700

    Time: 7 ms (62.04%) | Memory: 23.1 MB (93.43%) - LeetSync

--- 370-range-addition/range-addition.py
+++ 370-range-addition/range-addition.py
@@ -1,14 +1,16 @@
 class Solution:
     def getModifiedArray(self, length: int, updates: List[List[int]]) -> List[int]:
-
+        gradient_arr = [0]*(length+1)
         res = [0]*length
+
         for start,end,val in updates:
-            res[start]+=val
-            if end<length-1:
-                res[end+1]-=val
+            gradient_arr[start]+=val
+            gradient_arr[end+1]-=val
         
+        res[0]=gradient_arr[0]
+
         for i in range(1,length):
-            res[i]= res[i]+res[i-1]
+            res[i]= gradient_arr[i]+res[i-1]
   
         
         return res
