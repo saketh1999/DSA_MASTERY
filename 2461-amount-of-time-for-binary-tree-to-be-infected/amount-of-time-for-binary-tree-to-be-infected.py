ea8dde628e55860553a453dcad648d03412d60b5
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2025-06-29 16:35:36 -0500

    Time: 158 ms (74.58%) | Memory: 60.1 MB (57.83%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.py
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.py
@@ -1,43 +1,67 @@
-from collections import deque
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
     def amountOfTime(self, root: Optional[TreeNode], start: int) -> int:
-        parent_tree = {}
-        start_node = None
-
-        # Single DFS to build parent mapping and find start node
-        def dfs(node, parent):
+        
+        #construct Binary Parent Tree
+        
+        def construct_parent_tree(node,parent):
             nonlocal start_node
             if node is None:
                 return
             parent_tree[node] = parent
             if node.val == start:
                 start_node = node
-            dfs(node.left, node)
-            dfs(node.right, node)
 
-        dfs(root, None)
+            construct_parent_tree(node.left,node)
+            construct_parent_tree(node.right,node)
 
-        # BFS to simulate infection spread
-        visited = set()
-        queue = deque([start_node])
-        visited.add(start_node)
-        time = -1
+        
+        def time_to_infect(start):
+            time = -1
+            visited = set()
+            visited.add(start)
+            queue=deque([start])
 
-        while queue:
-            size = len(queue)
-            for _ in range(size):
-                node = queue.popleft()
-                for neighbor in (node.left, node.right, parent_tree[node]):
-                    if neighbor and neighbor not in visited:
-                        visited.add(neighbor)
-                        queue.append(neighbor)
-            time += 1
+            while queue:
+                size_of_queue = len(queue)
+                for _ in range(size_of_queue):
+                    node = queue.popleft()
+                    
+                    if node.left and node.left not in visited:
+                        queue.append(node.left)
+                        visited.add(node.left)
 
-        return time
+                    if node.right and node.right not in visited:
+                        queue.append(node.right)
+                        visited.add(node.right)
+
+                    if parent_tree[node]  and parent_tree[node] not in visited:
+                        queue.append(parent_tree[node])
+                        visited.add(parent_tree[node])
+
+                time+=1
+                    # visited = set()
+                    # queue = deque([start_node])
+                    # visited.add(start_node)
+                    # time = -1
+
+                    # while queue:
+                    #     size = len(queue)
+                    #     for _ in range(size):
+                    #         node = queue.popleft()
+                    #         for neighbor in (node.left, node.right, parent_tree[node]):
+                    #             if neighbor and neighbor not in visited:
+                    #                 visited.add(neighbor)
+                    #                 queue.append(neighbor)
+                    #     time += 1
+            return time
+                    
+        parent_tree = {}    
+        start_node = None
+        construct_parent_tree(root,None)
+        return time_to_infect(start_node)
