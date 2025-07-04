86daab993aef8537b521729a785001a6f3c5c351
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2025-07-04 12:08:40 -0500

    Time: 14 ms (87.74%) | Memory: 17.9 MB (62.45%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,42 +1,42 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s2) < len(s1):
+        if len(s1) > len(s2):
             return False
-
+        
         s1_dict = {}
         s2_dict = {}
 
-        small_start = ord('a')
-        small_end = ord('z')
+        start = ord('a')
+        end = ord('z')
 
-        #creating a directory of all characters
-        for i in range(small_start,small_end+1):
-            s1_dict[chr(i)] = 0
-            s2_dict[chr(i)] = 0
-        
-        len_s1 = len(s1)
+        for i in range(start,end+1):
+            s1_dict[chr(i)]=0
+            s2_dict[chr(i)]=0
 
-        for i in range(len_s1):
-            s1_dict[s1[i]] += 1
-            s2_dict[s2[i]] += 1
+        for i in range(len(s1)):
+            s1_dict[s1[i]]+=1
+            s2_dict[s2[i]]+=1
+
+        if s1_dict == s2_dict:
+            return True
         
- 
 
         l,r=0,len(s1)-1
+        print(len(s2))
         while r<len(s2):
             if s1_dict == s2_dict:
                 return True
-            
-            r+=1
-            if r<len(s2):
-                s2_dict[s2[r]] += 1
-            
             s2_dict[s2[l]]-=1
             l+=1
-            
+            r+=1
+            if r<len(s2):
+                s2_dict[s2[r]]+=1
 
         return False
-
-            
-            
         
+
+      
+        
+
+        
+                
