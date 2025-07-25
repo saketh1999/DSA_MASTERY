b8c36ca1db63a408256535834452432086e44ee7
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2025-07-25 10:27:42 -0500

    Time: 4 ms (71.06%) | Memory: 22.1 MB (64.19%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.py
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.py
@@ -1,21 +1,22 @@
 class Solution:
     def processStr(self, s: str) -> str:
-        res =[]
+        res =''
         for char in s:
             if char == '*':
                 if res:
-                    res.pop(-1)
+                    res = res[:-1]
             elif char == '#':
                 if res:
-                    dup = res.copy()
-                    for c in dup:
-                        res.append(c)
+                    # dup = res.copy()
+                    # for c in dup:
+                    #     res.append(c)
+                    res+=res
 
             elif char == '%':
                 if res:
                     res=res[::-1]
 
             else:
-                res.append(char)
+                res+=char
 
-        return "".join(res)
+        return res
