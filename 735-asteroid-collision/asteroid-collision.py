60813b3d29ef96bc540262c838de854e394440a3
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2025-08-24 22:24:18 -0700

    Time: 8 ms (27.53%) | Memory: 18.8 MB (61.51%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -3,19 +3,17 @@ class Solution:
         stack = []
 
         for i in range(len(asteroids)):
-
-            if asteroids[i]>0:
-                    stack.append(asteroids[i])
-
+            if len(stack)==0:
+                stack.append(asteroids[i])
             else:
-                while stack and stack[-1]>0 and abs(asteroids[i])>stack[-1]:
-                        stack.pop(-1)
-        
-                if  stack and abs(asteroids[i]) == stack[-1]:
-                        stack.pop()
-
-                elif not stack or stack[-1]<0: #this is important - it will also allow you to insert negative values
+                if stack and asteroids[i]>0:
                     stack.append(asteroids[i])
+                else:
+                    while stack and stack[-1]>0 and stack[-1]<abs(asteroids[i]):
+                        stack.pop()
+                    if stack and stack[-1] == abs(asteroids[i]):
+                        stack.pop()
+                    elif not stack or stack[-1]<0:
+                        stack.append(asteroids[i])
 
         return stack
-
