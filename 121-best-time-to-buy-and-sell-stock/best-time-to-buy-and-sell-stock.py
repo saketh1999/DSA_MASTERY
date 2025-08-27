36f077c20360e7b9c46d8ab76d50252c567b7f05
Sri Sai Saketh <48793201+saketh1999@users.noreply.github.com>
2025-08-27 12:02:21 -0700

    Time: 107 ms (39.29%) | Memory: 26.9 MB (32.21%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,10 +1,24 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        max_profit=0
-        prev_day_min = prices[0]
-        for i in range(1,len(prices)):
-            max_profit = max(max_profit,prices[i]-prev_day_min)
-            prev_day_min = min(prev_day_min,prices[i])
+        # max_profit=0
+        # prev_day_min = prices[0]
+        # for i in range(1,len(prices)):
+        #     max_profit = max(max_profit,prices[i]-prev_day_min)
+        #     prev_day_min = min(prev_day_min,prices[i])
 
+        # return max_profit
+        stack = []
+        max_profit=0
+        for i in range(len(prices)-1,-1,-1):
+            if not stack:
+                stack.append(prices[i])
+                # print("here",stack)
+            else:
+                if stack[-1] > prices[i]:
+                    max_profit = max(max_profit,stack[-1]-prices[i])
+                else:
+                    stack.pop()
+                    stack.append(prices[i])
+                # print(stack)
         return max_profit
 
